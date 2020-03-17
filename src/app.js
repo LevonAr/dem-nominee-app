5c089285c83d00232a67b86db7bad097b08e62f2
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-03-16 22:50:24 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -8,3 +8,6 @@ class App extends React.Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
