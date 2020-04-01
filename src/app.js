f9ba92e61cfc324e4a1b327f3819feb74ddbaf7f
LevonAr <37823213+LevonAr@users.noreply.github.com>
2020-03-31 23:24:23 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,6 @@ class App extends React.Component {
           <a
             className="App-link"
             href="https://reactjs.org"
-            target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
